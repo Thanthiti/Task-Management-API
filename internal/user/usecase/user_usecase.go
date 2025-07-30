989ca0c4c19ee94a32ab77a3d3157eb2eb2c6afe
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-07-30 20:18:32 +0700

    feat(user) : complete UserUsecase implementation

--- internal/user/usecase/user_usecase.go
+++ internal/user/usecase/user_usecase.go
@@ -54,8 +54,8 @@ func (uc *UserusecaseImpl) CreateUser(user model.User) error{
 	
 	// Save to DB 
 	if err := uc.repo.Save(user) ;err != nil {
-		logger.Log.Warn()
-		return  err
+		logger.Log.Error("Enable save user : ",err)
+		return  fmt.Errorf("enable save user : %v",err)
 	}
 
 	logger.Log.Info("User created : ", user.Email)
@@ -64,15 +64,63 @@ func (uc *UserusecaseImpl) CreateUser(user model.User) error{
 
 func (uc *UserusecaseImpl) Login(email ,password string) (string,error){
 	
-	return  "",nil
+	user , err := uc.repo.FindByEmail(email)
+	if err != nil || user == nil {
+		logger.Log.Warn("Login failed : email not found")
+		return "",fmt.Errorf("invalid credentials")
+	}
+
+	// Decode password in database and compare 
+	if !uc.cypto.ComparePassword(user.Password,password){
+		logger.Log.Warn("Login failed : incorect password")
+		return "",fmt.Errorf("invalid credentials")
+	}
+	
+	// Generate Token
+	token,err := uc.token.GenerateToken(user.ID)
+	if err != nil {
+		logger.Log.Error("Token generation failed : ",err)
+		return "",fmt.Errorf("invalid credentials")
+		
+	}
+	
+	logger.Log.Info("User logged in : ",email)
+	return  token,nil
 }
 
 func (uc *UserusecaseImpl) UpdateUser(user model.User) error{
 
+	exitUser,err := uc.repo.FindByID(user.ID)
+	if err != nil || exitUser == nil {
+		logger.Log.Warn("Update failed: user not found")
+		return fmt.Errorf("user not found")
+	}
+
+	exitUser.Name = user.Name
+	exitUser.Email = user.Email
+
+	if err := uc.repo.Update(*exitUser);err != nil {
+		logger.Log.Error("Update failed : ", err)
+		return err
+	}
+
+	logger.Log.Info("User update : ",user.ID)
 	return  nil
 }
 
 func (uc *UserusecaseImpl) DeleteUser(userID uint) error{
+	user, err := uc.repo.FindByID(userID)
+	if err != nil || user == nil {
+		logger.Log.Warn("Delete failed: user not found")
+		return fmt.Errorf("user not found")
+	}
+	
+	if err := uc.repo.Delete(userID);err != nil {
+		logger.Log.Error("Delete failed : ",err)
+		return err
+	}
+	
+	logger.Log.Info("User deleted : ",userID)
 	return nil
 }
 
