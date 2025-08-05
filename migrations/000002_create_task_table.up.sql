1be13051ad0c26b2740d0330f3babcec9bec0725
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-08-05 15:26:54 +0700

    chore: rename table and change database config

--- migrations/000002_create_task_table.up.sql
+++ migrations/000002_create_task_table.up.sql
@@ -1,9 +1,10 @@
-CREATE TABLE task (
+CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     title TEXT NOT NULL,
     description TEXT,
     status TEXT DEFAULT 'pending',
+    due_date TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP
